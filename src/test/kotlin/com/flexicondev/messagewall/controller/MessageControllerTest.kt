ddea30a03f96dd2227f0399e654ef3fc64388544
Mike Repec <mikerepec@gmail.com>
2022-04-11 20:37:27 +0200

    Test POST messages endpoint

--- src/test/kotlin/com/flexicondev/messagewall/controller/MessageControllerTest.kt
+++ src/test/kotlin/com/flexicondev/messagewall/controller/MessageControllerTest.kt
@@ -1,5 +1,9 @@
 package com.flexicondev.messagewall.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.flexicondev.messagewall.model.MessagePayload
+import com.flexicondev.messagewall.repository.MessageRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -7,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,7 +20,18 @@ internal class MessageControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    lateinit var messageRepository: MessageRepository
+
     val baseUrl = "/messages"
+
+    @BeforeEach
+    fun beforeEach() {
+        messageRepository.deleteAll()
+    }
 
     @Test
     fun `GET should return empty list of messages`() {
@@ -25,6 +41,23 @@ internal class MessageControllerTest {
                 content { contentType(MediaType.APPLICATION_JSON) }
                 jsonPath("$") { isArray() }
                 jsonPath("$") { isEmpty() }
+            }
+    }
+
+    @Test
+    fun `POST should create a new message`() {
+        val payload = MessagePayload("Test message", "Johnny Test")
+
+        mockMvc.post(baseUrl) {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(payload)
+            accept = MediaType.APPLICATION_JSON
+        }
+            .andExpect {
+                status { isCreated() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$.text") { value(payload.text) }
+                jsonPath("$.author") { value(payload.author) }
             }
     }
 }
