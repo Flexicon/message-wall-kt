d2f25721239a15e3c659bd8c09ec1b0b798a8391
Mike Repec <mikerepec@gmail.com>
2022-04-15 20:05:56 +0200

    Add CORS to API

--- src/main/kotlin/com/flexicondev/messagewall/MessageWallApplication.kt
+++ src/main/kotlin/com/flexicondev/messagewall/MessageWallApplication.kt
@@ -2,9 +2,25 @@ package com.flexicondev.messagewall
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
-class MessageWallApplication
+class MessageWallApplication {
+
+	@Bean
+	fun corsConfigurer(): WebMvcConfigurer {
+		return object : WebMvcConfigurer {
+			override fun addCorsMappings(registry: CorsRegistry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+			}
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<MessageWallApplication>(*args)
