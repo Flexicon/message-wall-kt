18a62c130c5740d783e73cce9f54dbfaade2df56
Mike Repec <mikerepec@gmail.com>
2022-04-15 20:20:06 +0200

    Add Cors Configuration via dedicated config class

--- src/main/kotlin/com/flexicondev/messagewall/MessageWallApplication.kt
+++ src/main/kotlin/com/flexicondev/messagewall/MessageWallApplication.kt
@@ -2,25 +2,10 @@ package com.flexicondev.messagewall
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @SpringBootApplication
-class MessageWallApplication {
-
-	@Bean
-	fun corsConfigurer(): WebMvcConfigurer {
-		return object : WebMvcConfigurer {
-			override fun addCorsMappings(registry: CorsRegistry) {
-				registry.addMapping("/**")
-					.allowedOrigins("*")
-					.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
-			}
-		}
-	}
-}
+class MessageWallApplication
 
 fun main(args: Array<String>) {
 	runApplication<MessageWallApplication>(*args)
