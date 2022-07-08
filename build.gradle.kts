2b4ea776613ab4b1ee54f00154e451b1afba06a9
Mike Repec <mikerepec@gmail.com>
2022-07-08 10:43:48 +0200

    Add and configure helm plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.unbrokendome.gradle.plugins.helm.command.internal.setFrom
 
 plugins {
     val kotlinVersion = "1.7.0"
@@ -8,6 +9,10 @@ plugins {
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+
+    val helmPluginVersion = "1.7.0"
+    id("org.unbroken-dome.helm") version helmPluginVersion
+    id("org.unbroken-dome.helm-releases") version helmPluginVersion
 }
 
 group = "com.flexicondev"
@@ -32,6 +37,30 @@ dependencies {
     val testContainersVersion = "1.17.3"
     testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
     testImplementation("org.testcontainers:mongodb:$testContainersVersion")
+}
+
+helm {
+    val projectChartName = project.name.replace("-", "")
+
+    charts {
+        create(projectChartName) {
+            chartName.set(projectChartName)
+            sourceDir.set(project.projectDir.resolve("ops/helm"))
+        }
+    }
+
+    releases {
+        create(projectChartName) {
+            from(chart(projectChartName))
+            version.set(project.version.toString())
+            valueFiles.from("ops/helm/values.yaml")
+            values.set(mapOf("projectRoot" to project.projectDir))
+        }
+    }
+}
+
+tasks.getByName("helmInstall") {
+    dependsOn("bootJar")
 }
 
 tasks.withType<KotlinCompile> {
