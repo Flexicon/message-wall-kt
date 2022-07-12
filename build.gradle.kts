84224e52a8dafa72e660c345cdbf70ca4edfdc6f
Mike Repec <mikerepec@gmail.com>
2022-07-12 22:23:28 +0200

    Include generated jooq classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,3 +53,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.generateJooqClasses {
+    outputDirectory.set(project.file("src/main/java/generated-jooq"))
+}
