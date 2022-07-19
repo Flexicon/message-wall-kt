be752906645d7ef834d61dd5ea01bae4902a41b3
Mike Repec <mikerepec@gmail.com>
2022-07-20 00:24:48 +0200

    Bump Kotlin and Spring Boot versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.7.0"
+    val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("org.springframework.boot") version "2.6.6"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("com.revolut.jooq-docker") version "0.3.7"
 }
